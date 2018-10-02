eeabbec81cb646be1d154594179bb38d0554639b
Geof Brownbridge <geof@Geofs-MacBook-Pro.local>
2018-10-02 15:33:24 +0100

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+let date = new moment();
+// date.add(15, 'year').subtract(9, 'months');
+
+// console.log(date.format('ddd Do MMM, YYYY'));
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -9,17 +9,19 @@ _socket.on('disconnect', function () {
 });
 
 _socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  // console.log('newMessage', message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 _socket.on('newLocationMessage', function (message) {
   var li = jQuery('<li></li>');
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var a = jQuery('<a target="_blank">my current location</a>');
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
